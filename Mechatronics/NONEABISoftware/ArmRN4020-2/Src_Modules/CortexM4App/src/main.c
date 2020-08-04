f6b1e56aa096198b2520284b63906325c8cb271c
ZhukovDematey <paul.avalosar@udlap.mx>
2020-08-04 13:52:58 -0500

    Changes

--- Mechatronics/NONEABISoftware/ArmRN4020-2/Src_Modules/CortexM4App/src/main.c
+++ Mechatronics/NONEABISoftware/ArmRN4020-2/Src_Modules/CortexM4App/src/main.c
@@ -9,20 +9,16 @@ int main(void)
   {
     //---------------------INCREASE CPU SPEED-------------------------
 
+    //The HSI16 is used as system clock source after startup from Reset
     //Flash erase and programming is only possible in voltage scale range 1
-
-    //RANGE 1 NORMAL MODE
-    //HSI status and default SWS
-    //HSE not bypassed
 
     //PWR_CR1 Voltage scaling range 1
     //PWR_CR5 Main regulator in range 1 normal mode
     //FLASH_ACR Instruction cache and data cache enabled, zero wait state*
     //RCC_CR All clocks off and not ready(hsi*), HSE not bypassed
-    //RCC_CFGR HSI16 selected as system clock, sysclk & PCLK1 & PCLK not divided, MCO output disabled 
-    //RCC_PLLCFGR
-    
-    //The HSI16 is used as system clock source after startup from Reset
+    //RCC_CFGR HSI16 selected as system clock, sysclk & PCLK1 & PCLK2 not divided, MCO output disabled 
+    //RCC_PLLCFGR PLLn mult by 8, PLLm div by 1, no PLL sourcce, PLLR div by 2, PLLR disabled    
+
     
     //Wait states for less than 90 MHz at VCore Range 1 normal mode
     FLASH->ACR |= FLASH_ACR_PRFTEN;
@@ -34,8 +30,8 @@ int main(void)
     RCC->CR |= RCC_CR_HSEON;
     while(!(RCC->CR & (RCC_CR_HSERDY)));
 
-    //Turn PLL, configure it and wait till ready
-    RCC->PLLCFGR = (RCC_PLLCFGR_PLLM_0 | (20 << RCC_PLLCFGR_PLLN_Pos)); //M = 0 , N = 20
+    //Turn PLL on, configure it and wait till ready
+    RCC->PLLCFGR = (RCC_PLLCFGR_PLLM_0 | (20 << RCC_PLLCFGR_PLLN_Pos)); //M = 2 , N = 20
     RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;    
     
     RCC->CR |= RCC_CR_PLLON;
