abfa237053f5c064af8692b6876e7a6ff0ae38ec
ZhukovDematey <paul.avalosar@udlap.mx>
2020-08-25 21:09:38 -0500

    Added encoder mode

--- Mechatronics/NONEABISoftware/EncoderBoard2/Src_Modules/CortexM4App/src/main.c
+++ Mechatronics/NONEABISoftware/EncoderBoard2/Src_Modules/CortexM4App/src/main.c
@@ -8,6 +8,54 @@ char receiveBuffer[UART_RX_BUFFER_LEN] = {0};
 
 void printString(const char myString[]);
 
+char* reverse(char *buffer, int i, int j);
+char* itoa(int value, char* buffer, int base);
+
+// inline function to swap two numbers
+static inline void myswap(char *x, char *y) {
+	char t = *x; *x = *y; *y = t;
+}
+
+// function to reverse buffer[i..j]
+char* reverse(char *buffer, int i, int j){
+	while (i < j)
+		myswap(&buffer[i++], &buffer[j--]);
+	return buffer;
+}
+
+// Iterative function to implement itoa() function in C
+char* itoa(int value, char* buffer, int base){
+	// invalid input
+	if (base < 2 || base > 32)
+		return buffer;
+
+	// consider absolute value of number
+	int n = value;
+
+	int i = 0;
+	while (n)
+	{
+		int r = n % base;
+		if (r >= 10) 
+			buffer[i++] = 65 + (r - 10);
+		else
+			buffer[i++] = 48 + r;
+		n = n / base;
+	}
+
+	// if number is 0
+	if (i == 0)
+		buffer[i++] = '0';
+	// If base is 10 and value is negative, the resulting string 
+	// is preceded with a minus sign (-)
+	// With any other base, value is always considered unsigned
+	if (value < 0 && base == 10)
+		buffer[i++] = '-';
+	buffer[i] = '\0'; // null terminate string
+	// reverse the string and return it
+	return reverse(buffer, 0, i - 1);
+}
+
 int main(void)
 {
   //OPTION BYTES
@@ -64,6 +112,7 @@ int main(void)
   
   RCC->CCIPR = (RCC->CCIPR & (~RCC_CCIPR_USART1SEL)) | (0b01 << RCC_CCIPR_USART1SEL_Pos);//System clock as USART1 clock
   RCC->CCIPR = (RCC->CCIPR & (~RCC_CCIPR_I2C2SEL)) | (0b01 << RCC_CCIPR_I2C2SEL_Pos);//System clock as I2C2 clock
+  RCC->CCIPR = (RCC->CCIPR & (~RCC_CCIPR_LPTIM1SEL)) | (0b00 << RCC_CCIPR_LPTIM1SEL_Pos);//PCLK selected as LPTIM1 clock
 
   //---------------------CONFIGURE GPIO---------------------
   
@@ -93,14 +142,14 @@ int main(void)
   GPIOA->OTYPER |= GPIO_OTYPER_OT8; //Open drain
   GPIOA->OSPEEDR = (GPIOA->OSPEEDR & (~GPIO_OSPEEDR_OSPEED8)) | (0b00 << GPIO_OSPEEDR_OSPEED8_Pos); //Low speed
   GPIOA->PUPDR = (GPIOA->PUPDR & (~GPIO_PUPDR_PUPD8)) | (0b01 << GPIO_PUPDR_PUPD8_Pos); //Pull up
-  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL8)) | (4 << GPIO_AFRH_AFSEL8_Pos); //Alternate function 4
+  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL8)) | (4 << GPIO_AFRH_AFSEL8_Pos); //Alternate function 4 
 
   //PA9 I2C2_SCL
   GPIOA->MODER = (GPIOA->MODER & (~GPIO_MODER_MODE9)) | (0b10 << GPIO_MODER_MODE9_Pos); //Alternate function mode
   GPIOA->OTYPER |= GPIO_OTYPER_OT9; //Open drain
   GPIOA->OSPEEDR = (GPIOA->OSPEEDR & (~GPIO_OSPEEDR_OSPEED9)) | (0b00 << GPIO_OSPEEDR_OSPEED9_Pos); //Low speed
   GPIOA->PUPDR = (GPIOA->PUPDR & (~GPIO_PUPDR_PUPD9)) | (0b01 << GPIO_PUPDR_PUPD9_Pos); //Pull up
-  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL9)) | (4 << GPIO_AFRH_AFSEL9_Pos); //Alternate function 4
+  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL9)) | (4 << GPIO_AFRH_AFSEL9_Pos); //Alternate function 4 
 
   //UART1 GPIO CONFIGURATION
   //PA10 UART1_RX
@@ -108,14 +157,37 @@ int main(void)
   GPIOA->OTYPER &= (~GPIO_OTYPER_OT10);//Push pull  
   GPIOA->OSPEEDR = (GPIOA->OSPEEDR & (~GPIO_OSPEEDR_OSPEED10)) | (0b00 << GPIO_OSPEEDR_OSPEED10_Pos); //Low speed
   GPIOA->PUPDR = (GPIOA->PUPDR & (~GPIO_PUPDR_PUPD10)) | (0b00 << GPIO_PUPDR_PUPD10_Pos); //No pull up, no pull down
-  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL10)) | (7 << GPIO_AFRH_AFSEL10_Pos); //Alternate function 10
+  GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL10)) | (7 << GPIO_AFRH_AFSEL10_Pos); //Alternate function 7 
 
   //PB6 UART1_TX
   GPIOB->MODER = (GPIOB->MODER & (~GPIO_MODER_MODE6)) | (0b10 << GPIO_MODER_MODE6_Pos) ; //Alternate function mode
   GPIOB->OTYPER &= (~GPIO_OTYPER_OT6);//Push pull  
   GPIOB->OSPEEDR = (GPIOB->OSPEEDR & (~GPIO_OSPEEDR_OSPEED6)) | (0b00 << GPIO_OSPEEDR_OSPEED6_Pos); //Low speed
   GPIOB->PUPDR = (GPIOB->PUPDR & (~GPIO_PUPDR_PUPD6)) | (0b00 << GPIO_PUPDR_PUPD6_Pos); //No pull up, no pull down
-  GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL6)) | (7 << GPIO_AFRL_AFSEL6_Pos); //Alternate function 10
+  GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL6)) | (7 << GPIO_AFRL_AFSEL6_Pos); //Alternate function 7 
+
+  //LPTIM1 GPIO CONFIGURATION
+  //PB5 LPTIM_IN1
+  GPIOB->MODER = (GPIOB->MODER & (~GPIO_MODER_MODE5)) | (0b10 << GPIO_MODER_MODE5_Pos) ; //Alternate function mode
+  GPIOB->OTYPER &= (~GPIO_OTYPER_OT5);//Push pull  
+  GPIOB->OSPEEDR = (GPIOB->OSPEEDR & (~GPIO_OSPEEDR_OSPEED5)) | (0b00 << GPIO_OSPEEDR_OSPEED5_Pos); //Low speed
+  GPIOB->PUPDR = (GPIOB->PUPDR & (~GPIO_PUPDR_PUPD5)) | (0b00 << GPIO_PUPDR_PUPD5_Pos); //No pull up, no pull down
+  GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL5)) | (11 << GPIO_AFRL_AFSEL5_Pos); //Alternate function 11 
+
+  //PB7 LPTIM_IN2
+  GPIOB->MODER = (GPIOB->MODER & (~GPIO_MODER_MODE7)) | (0b10 << GPIO_MODER_MODE7_Pos) ; //Alternate function mode
+  GPIOB->OTYPER &= (~GPIO_OTYPER_OT7);//Push pull  
+  GPIOB->OSPEEDR = (GPIOB->OSPEEDR & (~GPIO_OSPEEDR_OSPEED5)) | (0b00 << GPIO_OSPEEDR_OSPEED7_Pos); //Low speed
+  GPIOB->PUPDR = (GPIOB->PUPDR & (~GPIO_PUPDR_PUPD7)) | (0b00 << GPIO_PUPDR_PUPD7_Pos); //No pull up, no pull down
+  GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL7)) | (11 << GPIO_AFRL_AFSEL7_Pos); //Alternate function 11
+
+  //ENC ENABLE CONFIGURATION
+  //PB9 
+  GPIOB->MODER = (GPIOB->MODER & (~GPIO_MODER_MODE9)) | (0b10 << GPIO_MODER_MODE9_Pos) ; //General purpose outputMode
+  GPIOB->OTYPER &= (~GPIO_OTYPER_OT9);//Push pull  
+  GPIOB->OSPEEDR = (GPIOB->OSPEEDR & (~GPIO_OSPEEDR_OSPEED5)) | (0b00 << GPIO_OSPEEDR_OSPEED7_Pos); //Low speed
+  GPIOB->PUPDR = (GPIOB->PUPDR & (~GPIO_PUPDR_PUPD7)) | (0b00 << GPIO_PUPDR_PUPD7_Pos); //No pull up, no pull down
+  GPIOB->BSRR |= GPIO_BSRR_BR9;//Turn on encoder  
 
 
   //---------------------CONFIGURE I2C-------------------------
@@ -205,8 +277,23 @@ int main(void)
 
   DMAMUX1_Channel2->CCR = (DMAMUX1_Channel2->CCR & (~DMAMUX_CxCR_DMAREQ_ID)) | (19 << DMAMUX_CxCR_DMAREQ_ID_Pos);//Request 29
 
-  //DMA
-  for (int i = 0; i < 10000; i++);  
+  //---------------------CONFIGURE LPTIM-------------------------
+
+  RCC->APB1ENR1 |= RCC_APB1ENR1_LPTIM1EN;//Enable LPTIM1 clock
+
+  //LPTIM1 CONFIGURATION
+  LPTIM1->CFGR = (LPTIM1->CFGR & (~LPTIM_CFGR_CKFLT)) | (0b11 << LPTIM_CFGR_CKFLT_Pos);//8 clock periods for valid transition
+  LPTIM1->CFGR &= ~LPTIM_CFGR_CKSEL;////LPTIM clocked by internal clock source
+  LPTIM1->CFGR = (LPTIM1->CFGR & (~LPTIM_CFGR_CKPOL)) | (0b10 << LPTIM_CFGR_CKPOL_Pos);//both edges are active edges
+  LPTIM1->CFGR |= LPTIM_CFGR_COUNTMODE;//External input 1
+  LPTIM1->CFGR |= LPTIM_CFGR_ENC;//Encoder mode enable
+  LPTIM1->CR |= LPTIM_CR_ENABLE;//LPTIM1 Enable
+  LPTIM1->ICR |= LPTIM_ICR_ARROKCF;
+  LPTIM1->ARR = 65535;
+  while(!(LPTIM1->ISR & LPTIM_ISR_ARROK));
+  LPTIM1->CR |= LPTIM_CR_CNTSTRT; //Timer start in Continuous mode
+
+
   
   uint32_t g = 0;
 
@@ -216,9 +303,11 @@ int main(void)
   lcd_puts("Encoder");
   lcd_gotoxy(1,7);
   lcd_puts("Saint Germain");
+
+  char buffer[21];
   while (1)
   {
-
+    /*
     printString("\x31""Hola\n");
 
     for(int i = 1; i < 1000000;i++);
@@ -230,6 +319,29 @@ int main(void)
       receiveBuffer[g] = 0;
       g = (g+1) % UART_RX_BUFFER_LEN;
     }
+    */
+
+    uint32_t count=LPTIM1->CNT;
+    count=LPTIM1->CNT;
+    
+    itoa(count,buffer,10);
+    lcd_gotoxy(1,5);
+    lcd_puts(buffer);
+
+    static int i = 0;
+    if(i == 0){
+      RCC->CCIPR = (RCC->CCIPR & (~RCC_CCIPR_LPTIM1SEL)) | (0b10 << RCC_CCIPR_LPTIM1SEL_Pos);//PCLK selected as LPTIM1 clock
+      i = 1;
+    }else{
+      RCC->CCIPR = (RCC->CCIPR & (~RCC_CCIPR_LPTIM1SEL)) | (0b00 << RCC_CCIPR_LPTIM1SEL_Pos);//PCLK selected as LPTIM1 clock
+      i = 0;
+    }
+
+    itoa(g++,buffer,10);
+    lcd_gotoxy(1,3);
+    lcd_puts(buffer);
+
+
     
     /*
     if(USART1->ISR & USART_ISR_RXNE){
